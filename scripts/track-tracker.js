842a8bc3dba30734f8dbedba3bca581d256c5e3a
CarterDC <carter_dc99@hotmail.com>
2021-06-17 20:59:57 +0200

    sigh

--- scripts/track-tracker.js
+++ scripts/track-tracker.js
@@ -29,17 +29,4 @@ function RegisterHandlebarsHelpers(){
 async function ttDoSomething() {
   console.log('%cTrack Tracker %c| Doing Some Shit...', 'color: #22eeee', 'color: #fff');
   
-  /*
-  let path = "modules/track-tracker/templates/soundPartial.hbs";
-
-  await new Promise((resolve, reject) => {
-    game.socket.emit('template', path, resp => {
-      if ( resp.error ) return reject(new Error(resp.error));
-      const compiled = Handlebars.compile(resp.html);
-      Handlebars.registerPartial("soundPartial", compiled);
-      _templateCache[path] = compiled;
-      console.log(`Foundry VTT | Retrieved and compiled template ${path}`);
-      resolve(compiled);
-    });
-  });*/
 };
