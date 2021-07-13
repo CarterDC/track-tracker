55280b007787bdf49700a05835be4da428c03fb8
CarterDC <carter_dc99@hotmail.com>
2021-07-13 23:36:15 +0200

    Daily Update

--- scripts/tt-playlist-sound.js
+++ scripts/tt-playlist-sound.js
@@ -7,13 +7,31 @@
     super(data, context);
   }
 
-  /** @override */
+  /**
+   * @override
+   * added removal of flags on path change
+   * + mark out schedule if update during playback
+   */
   _onUpdate(changed, options, userId) {
     super._onUpdate(changed, options, userId);
     if ( "path" in changed ) {
       this._removeTTFlags();
       if ( this.sound ) this.sound.stop();
       this.sound = this._createSound();
+    }
+    if ( "repeat" in changed || "flag" in changed ) {
+      if ( this.sound.playing ) {
+        //just in case bahavior changes while playing
+        const markin = this.getFlag("track-tracker", "markin");
+        const markout = this.getFlag("track-tracker", "markout");
+        if ( markout ) {
+          //schedule an anticipated ending
+          this.sound.schedule(this._markOut.bind(this), markout);
+        } else if ( this.data.repeat && markin ) {
+          //in case there's a markin, bypass the auto-reapeat
+          this.sound.schedule(this._markOut.bind(this), this.sound.duration - 0.1);
+        }
+      }
     }
     this.sync();
   }
@@ -33,6 +51,7 @@
   /**
    * Special handling that occurs when playback of a PlaylistSound is started.
    * @private
+   * @override added mark out schedule to end/loop a sound prematurely
    */
    async _onStart() {
     if ( !this.playing ) return this.sound.stop();
@@ -62,6 +81,8 @@
 
   /**
    * Synchronize playback for this particular PlaylistSound instance
+   * 
+   * @override adds mark in as offset
    */
    sync() {
     if ( !this.sound ) return;
@@ -74,18 +95,19 @@
       }
       else return this.sound.stop();
     }
-    console.log(`TT | sync`, this.sound);
     // Determine playback configuration
     const playback = {
       loop: this.data.repeat,
       volume: this.volume,
       fade: fade
     };
+
     const markin = this.getFlag("track-tracker", "markin");
+    const markout = this.getFlag("track-tracker", "markout");
     if ( markin && this.playing && !this.sound.playing ) {
+      //set mark in as playback offset (overwriten by pausedtime in next statement if needed)
       playback.offset = markin;
     }
-
     if ( this.data.pausedTime && this.playing && !this.sound.playing ) playback.offset = this.data.pausedTime;
 
     // Load and autoplay, or play directly if already loaded
@@ -96,19 +118,21 @@
   /**
    * Called by schedule to end or loop a sound before it's natural ending
    * in case of loop, start replay according to markin if exists.
-   * @param {PlaylistSound} sound     the sound that triggered the schedule
+   * @param {Sound} sound     the sound (not PlaylistSound) that triggered the schedule
    */
   _markOut(sound) {
     if ( !sound ) return;
     if ( !this.playing ) return;
-    if ( !this.data.repeat ) {
+    if ( this.sound.id !== sound.id ) return;
+    //console.log(`TT | _markout`, sound);
+    if ( ! sound.loop ) {
       //just stop and signal the playlist to deal with the rest
       sound.stop();
       return this._onEnd();
     } else {
-      sound.stop(); // note that .stop() also removes any scheduledEvents
+      sound.stop();
       const playback = {
-        loop: this.data.repeat,
+        loop: true,
         volume: this.volume
       };
       const markin = this.getFlag("track-tracker", "markin");
